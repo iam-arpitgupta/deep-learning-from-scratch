ac45164ed0123e2cae89e4eb504971db51810c8c
iam-arpitgupta <thearpitgupta2003@gmail.com>
2025-09-01 10:38:39 +0530

    update the broadcasting file

--- Broadcasting.py
+++ Broadcasting.py
@@ -1,4 +1,21 @@
 
+#Broadcasting role 
+matrix = [[1,2,3,5],
+           [4,7,9,3],
+           [9,7,6,5]]
+
+
+# sum 
+np.sum(matrix , axis = 0) #all rows , dim =3 
+np.sum(matrix , axis = 1) #all cols , dim =3 
+
+
+# with dim 
+np.sum(matrix , axis = 0 , keep_dim = True) #all rows , dim = 1 X 3 2D array
+np.sum(matrix , axis = 1 , keep_dim = True) #all col , dim =  3  X 1 2D array
+
+
+
 # broadcasting rules 
 import numpy as np 
 A = [[1, 2, 3], [4, 5, 6], [7, 8,9]]
@@ -18,3 +35,7 @@ print(np.sum(A, axis = 1,keepdims = True).shape)
 
 print(np.max(A, axis = 0))
 print(np.max(A, axis = 1))
+
+
+
+
--- classes_nn.py
+++ classes_nn.py
@@ -16,22 +16,3 @@ class nn:
 dense1 = nn(2,3)
 dense1.forward(X)
 
-
-#Broadcasting role 
-matrix = [[1,2,3,5],
-           [4,7,9,3],
-           [9,7,6,5]]
-
-
-# sum 
-np.sum(matrix , axis = 0) #all rows , dim =3 
-np.sum(matrix , axis = 1) #all cols , dim =3 
-
-
-# with dim 
-np.sum(matrix , axis = 0 , keep_dim = True) #all rows , dim = 1 X 3 2D array
-np.sum(matrix , axis = 1 , keep_dim = True) #all col , dim =  3  X 1 2D array
-
-
-
-
